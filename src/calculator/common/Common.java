d3744eb2d14ccf5d54c8abf82696c3dbaa2f8f5f
m_dsy <m_dsy@DESKTOP-TVDB2PP>
2017-06-06 19:55:19 +1000

    Fixed logical issue and removed redundant classes

--- src/calculator/common/Common.java
+++ src/calculator/common/Common.java
@@ -8,7 +8,7 @@ public class Common {
     public static String OPERAND_REGEX = "^[0-9]+[.]?[0-9]*";
     // Iteration 2+
     // public static String OPERATION_REGEX = "^[+|-|*|/|^|sin|cos|tan]$";
-    public static String OPERATION_REGEX = "^[+|-|*|/|^]$";
+    public static String OPERATION_REGEX = "^[\\+|\\-|\\*|/|\\^]$";
     
     // enum used to track what the current argument type is being validated
     public enum ArgTypes {
